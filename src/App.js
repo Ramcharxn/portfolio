5f2d71577b17f9a21ffd8297760e35d4a3db9a19
Ramcharxn <ramcharxn@gmail.com>
2021-02-15 19:29:31 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import './App.css';
 import Navbar from './Components/Navbar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 import Home from './Components/pages/Home'
 import Page2 from './Components/pages/Page2'
 
 function App() {
   return (
     <>  
-    <Router>
       <Navbar /> 
       <Switch>
-        <Route path='/' exact component={Home}/>
+        <Route exact path='/' component={Home}/>
       </Switch>
       <Switch>
-        <Route path='/services' exact component={Page2}/>
+        <Route exact path='/services' component={Page2}/>
       </Switch>
-    </Router> 
     </>
   );
 }
